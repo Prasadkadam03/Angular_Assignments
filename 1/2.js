acafa72498f8ce7e9f893738a26b9306972bf5a9
Prasadkadam03 <prasadkadam29503@gmail.com>
2023-12-16 23:05:58 +0530

    fuctions in Typescript

--- 1/2.js
+++
@@ -1,7 +0,0 @@
-var PI = 3.14;
-var Ret = 0;
-function Area(Radius) {
-    return PI * Radius * Radius;
-}
-Ret = Area(5);
-console.log("Area of circle is : " + Ret);
--- 1/3.js
+++
@@ -1,9 +0,0 @@
-function DisplayFactors(Value) {
-    var i = 0;
-    for (i = 1; i <= (Value / 2); i++) {
-        if ((Value % i) == 0) {
-            console.log(i);
-        }
-    }
-}
-DisplayFactors(20);
